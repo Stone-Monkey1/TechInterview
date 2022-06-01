85592c6646df18db14de26fe45d26070c857a027
Stone-Monkey1 <92111289+Stone-Monkey1@users.noreply.github.com>
2022-06-01 13:28:19 -0400

    Init

--- src/App.js
+++ src/App.js
@@ -1,25 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from 'react';
+import { Container, Grow, Grid } from '@material-ui/core';
+import { useDispatch } from 'react-redux';
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import {getPosts} from './actions/posts';
+import Posts from './components/Posts/Posts';
+import Form from './components/Form/Form';
+import useStyles from './styles';
+import Navbar from './components/Navbar/Navbar';
+import Home from './components/Home/Home';
+
+const App = () => {
+
+    return (
+        <Container maxWidth="lg">
+            <Navbar />
+            <Home />
+        </Container>
+    );
 }
 
 export default App;
---
+++ src/components/Navbar/Navbar.js
@@ -0,0 +1,42 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import { AppBar, Avatar, Typography, Button, Toolbar } from "@material-ui/core";
+import useStyles from "./styles";
+
+const Navbar = () => {
+  const classes = useStyles();
+
+  const user = null;
+
+  return (
+    <AppBar className={classes.appBar} position="static" color="inherit">
+      <div className={classes.brandContainer}>
+        <Typography
+          component={Link}
+          to="/"
+          className={classes.heading}
+          variant="h2"
+        />
+
+        <Typography className={classes.heading} variant="h2" align="center">
+          LWI Tech Interview
+        </Typography>
+        <img src="" alt="" height="60" />
+      </div>
+      <Toolbar className={classes.toolbar}>
+        {user ? (
+            <div className={classes.profile}>
+                <Avatar className={classes.purple} alt={user.result.name} src={user.result.imageUrl}>{user.result.name.charAt(0)}</Avatar>
+                <Typography className={classes.userName} variant="h6">{user.result.name}</Typography>
+                <Button variant="contained" className={classes.logout} color="secondary">Logout</Button>
+            </div>
+
+            ) : (
+            <Button component={Link} to="/auth" variant="contained" color="primary">Sign In</Button>
+        )}
+      </Toolbar>
+    </AppBar>
+  );
+};
+
+export default Navbar;
